a407eb28fdab0070dc81c9b5790f4d286388bf9e
saadbinmunir21 <saadbinmunir21@gmail.com>
2025-11-19 20:21:45 +0500

    final update

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // change to your deployed backend URL later
+  baseURL: "https://ledger-backend-yn93.onrender.com/api", // change to your deployed backend URL later
 });
 
 export default api;
 
-//https://ledger-backend-yn93.onrender.com/api
+//
